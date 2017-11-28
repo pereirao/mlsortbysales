2d6fa6f3896ae83c2a35e0942c99612838ac40f3
Alexandre Pereira <alempereira@hotmail.com>
2017-11-28 11:03:23 -0200

    Create mlsortbysales.user.js

---
+++ src/mlsortbysales.user.js
@@ -0,0 +1,64 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://*.mercadolivre.com.br/*
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js
+// @grant        none
+// ==/UserScript==
+
+var mlsort = {
+
+    qty: function(e) {
+        var q = 0;
+        if(e.search("vendido")) {
+            q = parseInt(e.split(" ")[1]);
+            q = q > 0 ? q : 0;
+        }
+        return q;
+    },
+
+    sort: function() {
+        var q = $(".results-item").length;
+        while(q > 0) {
+            for(j = 0; j < q; j++) {
+                var m = mlsort.qty($(".results-item:eq("+j+") .item__condition").text());
+                var n = mlsort.qty($(".results-item:eq("+(j+1)+") .item__condition").text());
+                if(m < n) {
+                    $(".results-item:eq("+j+")").insertAfter($(".results-item:eq("+(j+1)+")"));
+                }
+            }
+            q--;
+        }
+    },
+
+    init: function() {
+        if($(".results-item").length > 0) {
+            $('<div/>', {
+                id: 'sortBySales',
+                text: 'Vendidos',
+                css: {
+                    'position': 'fixed',
+                    'top': '95vh',
+                    'left': '95vw',
+                    'display': 'inline-block',
+                    'padding': '10px',
+                    'background-color': '#FFF059',
+                    'color': 'black',
+                    'font-size': '1em',
+                    'border-radius': '8px',
+                    'cursor': 'pointer'
+                }
+            }).appendTo('.nav-header').on('click', function() {
+                mlsort.sort();
+            });
+        }
+    },
+};
+
+$(document).ready(function() {
+    mlsort.init();
+});
+
